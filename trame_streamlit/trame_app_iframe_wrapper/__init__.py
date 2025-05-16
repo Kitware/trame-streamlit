38262ca5495dfa32abec0ae165cf365fb2aa0cd5
Lucie Macron <lucie.macron@kitware.com>
2025-05-16 16:35:18 +0200

    Fix build directory

--- trame_streamlit/trame_app_iframe_wrapper/__init__.py
+++ trame_streamlit/trame_app_iframe_wrapper/__init__.py
@@ -2,7 +2,7 @@ import streamlit.components.v1 as components
 import os
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
-build_dir = os.path.join(parent_dir, "frontend/build")
+build_dir = os.path.join(parent_dir, "frontend/dist")
 
 trame_app_iframe_wrapper = components.declare_component(
     "trame_app_iframe_wrapper",
